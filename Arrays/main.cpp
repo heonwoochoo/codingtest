ddc61ad43d722ed23559a76c61b757ee103ce09f
heonwoochoo <cngjsdn9@gmail.com>
2023-05-10 11:15:00 +0900

    binary search

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include "search.h"
 
 int main()
 {
-	std::cout << "hellow world" << std::endl;
+	std::vector<int> nums = { 2,1,3,5,6,7 };
+	const int target = 7;
+
+	std::cout << Solution::binary_search(nums, target);
 
 	return 0;
 }
---
+++ Arrays/search.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+
+	static int binary_search(std::vector<int>& nums, int target)
+	{
+		int l_idx = 0;
+		int r_idx = nums.size();
+
+		while (l_idx <= r_idx)
+		{
+			int pivot_idx = (l_idx + r_idx) / 2;
+
+			int pivot = nums[pivot_idx];
+			if (pivot == target)
+			{
+				return pivot_idx;
+			}
+			else if (pivot < target)
+			{
+				// l을 pivot의 한칸 오른쪽으로 옮긴다
+				l_idx = pivot_idx + 1;
+			}
+			else // pivot > target
+			{
+				// r을 pivot의 한칸 왼쪽으로 옮긴다
+				r_idx = pivot_idx - 1;
+			}
+		}
+		return -1;
+	}
+};
